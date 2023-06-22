002ecd8b8b7088e28a5c887a9cb5e32228efe31b
AR-MONSTER <135571765+AR-MONSTER@users.noreply.github.com>
2023-06-22 15:00:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['919526718775', 'PSYCHO MONSTER', true],
+  ['919526718775', 'Ash', true], 
   [''] 
 ] //Number of owners
 
